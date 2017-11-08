3de7eb00993f4c24ee667fabe86b99048f70f8c2
Todd Austin <austin.todd.j@gmail.com>
2017-11-07 18:34:52 -0600

    Update the resource directory

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -70,10 +70,10 @@
         <div class="top-right links">
             @auth
                 <a href="{{ url('/home') }}">Home</a>
-                @else
-                    <a href="{{ route('login') }}">Login</a>
-                    <a href="{{ route('register') }}">Register</a>
-                    @endauth
+            @else
+                <a href="{{ route('login') }}">Login</a>
+                <a href="{{ route('register') }}">Register</a>
+            @endauth
         </div>
     @endif
 
